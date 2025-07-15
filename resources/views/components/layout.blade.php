3985a427e79b824b5c2309d102ad9c1a0ce67b2d
KiraQiez <beefkira@gmail.com>
2025-07-15 19:33:55 +0800

    edit new layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,35 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>CampusOne Dashboard</title>
+  <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" />
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
 </head>
 <body>
-    <!-- Navigation Sidebar -->
-    <nav class="sidebar">
-        <div class="sidebar-header">
-            <h4>CampusOne</h4>
-        </div>
-        <ul class="items-list">
-            <li><a href="#"><i class="fas fa-home"></i> Home</a></li>
-            <li><a href="#"><i class="fas fa-book"></i> Courses</a></li>
-            <li><a href="#"><i class="fas fa-users"></i> Students</a></li>
-            <li><a href="#"><i class="fas fa-clipboard-list"></i> Exams</a></li>
-        </ul>
-
-    </nav>
-    <!-- Main Content -->
-    <div class="container mt-5">
-        {{ $slot }}
+  <!-- Navigation Sidebar -->
+  <nav class="sidebar">
+    <div class="sidebar-header">
+      <h4>CampusOne</h4>
     </div>
-</body>
-</html>
+    <ul class="items-list">
+      <li><a href="#"><i class="fas fa-home"></i> Home</a></li>
+      <li><a href="#"><i class="fas fa-book"></i> Courses</a></li>
+      <li><a href="#"><i class="fas fa-users"></i> Students</a></li>
+      <li><a href="#"><i class="fas fa-clipboard-list"></i> Exams</a></li>
+    </ul>
+  </nav>
 
-  
+  <!-- Main Content -->
+  <div class="main-content">
+    <div class="container-fluid py-4">
+      {{ $slot }}
+    </div>
+  </div>
 </body>
 </html>
